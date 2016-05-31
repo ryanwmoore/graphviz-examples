d80bcce9e025798e0a45625fb94fd57108612cc8
Ryan W. Moore <rwmoore07@gmail.com>
2016-05-31 09:58:10 -0400

    Programmatic Example

--- make.sh
+++ make.sh
@@ -1,5 +1,16 @@
+(
+  echo "digraph G {" &&
+  ps xo ppid,pid,cmd |
+    tail -n +2 |
+    awk 'BEGIN{OFS="->"} { print $1, $2; print $2 "[label=\"" $3 "\"]"}' &&
+        echo "1[label=\"init (PID #1)\"]"
+  echo "}"
+) > processes.dot
+
 for graph in *.dot
 do
-    dot -Tpdf "${graph}" > $(basename "${graph}" .dot).pdf
+    OUTNAME=$(basename "${graph}" .dot).pdf
+    echo "Processing ${graph} to ${OUTNAME}"
+    dot -Tpdf "${graph}" > "${OUTNAME}"
 done
 
